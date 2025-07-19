c9c0e748ff659b1c6b5b1ce0b90df301f2cd9aea
faiqalharits <alharistfaiq10@gmail.com>
2025-07-19 10:38:14 +0700

    sudah

--- Js/data.js
+++ Js/data.js
@@ -3,68 +3,79 @@ const armada = [
     name: "Avanza",
     image: "/assets/images/car/Toyota-Avanza.png",
     harga: "500.000",
-    seat: 6
+    seat: 6,
+    icon: "mdi:car-sedan"
   },
   {
     name: "Expander",
     image: "/assets/images/car/Mitsubishi-Xpander.png",
     harga: "650.000",
-    seat: 6
+    seat: 6,
+    icon: "mdi:car-sports-utility"
   },
   {
     name: "Innova Reborn",
     image: "/assets/images/car/innova-reborn.png",
-    harga: "750.000",
-    seat: 6
+    harga: "800.000",
+    seat: 6,
+    icon: "mdi:car-estate"
   },
   {
     name: "Innova Zenix",
     image: "/assets/images/car/Innova-Zenix.png",
-    harga: "950.000",
-    seat: 6
+    harga: "1.100.000",
+    seat: 6,
+    icon: "mdi:car-estate"
   },
   {
     name: "Fortuner",
     image: "/assets/images/car/Toyota-Fortuner.png",
     harga: "1.500.000",
-    seat: 6
+    seat: 6,
+    icon: "mdi:car-sports-utility"
   },
   {
     name: "Pajero",
     image: "/assets/images/car/Pajero-Sport.png",
     harga: "1.700.000",
-    seat: 6
+    seat: 6,
+    icon: "mdi:car-sports-utility"
   },
   {
     name: "Alphard Euro 4",
     image: "/assets/images/car/Alphard-Euro4.png",
     harga: "3.600.000",
-    seat: 6
+    seat: 6,
+    icon: "mdi:car-luxury"
   },
   {
     name: "Hiace Commuter",
     image: "/assets/images/car/Hiace-Commuter.png",
     harga: "1.550.000",
-    seat: 14
+    seat: 16,
+    icon: "mdi:van-passenger"
   },
   {
     name: "Hiace Premio",
     image: "/assets/images/car/Hiace-Premio.png",
     harga: "1.700.000",
-    seat: 12
+    seat: 12,
+    icon: "mdi:van-passenger"
   },
   {
     name: "Toyota Alphard",
     image: "/assets/images/car/Toyota-Alphard.png",
     harga: "2.600.000",
-    seat: 6
+    seat: 6,
+    icon: "mdi:car-luxury"
   },
   {
     name: "New Avanza Veloz",
     image: "/assets/images/car/Avanza-Veloz.png",
     harga: "650.000",
-    seat: 6
-  }
+    seat: 6,
+    icon: "mdi:car-sedan"
+  },
 ];
 
 function loadArmadaCards(limit) {
@@ -102,7 +113,8 @@ function loadArmadaCards(limit) {
         </div>
         <div class="price-order">
           <span class="price">Rp.${car.harga},-</span>
-          <a class="pesan-btn" href="https://wa.me/6289684887363?text=Halo, SELAMAT DATANG DI RRGaruda www.RRGaruda.com Ready Unit ${encodeURIComponent(car.name)} Melayani Tour&TreveL Tidak melayani lepas kunci" target="_blank">
+          <a class="pesan-btn" href="https://wa.me/6289684887363?text=Halo%2C%20SELAMAT%20DATANG%20DI%20RRGaruda%0Awww.RRGaruda.com%0AReady%20Unit%20%0A🚘%20${encodeURIComponent(car.name)}%0AMelayani%20Tour%20%26%20Travel%0ATidak%20melayani%20lepas%20kunci
+          " target="_blank">
             Pesan
           </a>
         </div>
